e4156e52a00aa68886e7391a9c2903ec5ef20b06
GuyKopel1568 <guykopel1568@gmail.com>
2024-12-28 15:30:44 +0200

    Complete x2 project

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function FlashCards() {
   const [selectedID, setSelectedID] = React.useState(2002);
 
   function handleClick(id) {
-    setSelectedID(id);
+    setSelectedID(id !== selectedID ? id : null);
   }
 
   return (
